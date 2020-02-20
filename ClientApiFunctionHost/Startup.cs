d1f7c8e539069492dfb2f706620751f609f17de4
Tona Diaz <tdiaz@healthwise.org>
2020-02-20 08:03:36 -0700

    One dependency away from glory.

--- ClientApiFunctionHost/Startup.cs
+++ ClientApiFunctionHost/Startup.cs
@@ -23,99 +23,6 @@ using System.Security.Claims;
 [assembly: FunctionsStartup(typeof(ClientApiFunctionHost.Startup))]
 namespace ClientApiFunctionHost
 {
-    public static class HackyAuthenticationExtensions
-    {
-        public static AuthenticationBuilder AddAuthentication(this IServiceCollection services)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            services.AddAuthenticationCore();
-            services.AddWebEncoders();
-            services.TryAddSingleton<ISystemClock, SystemClock>();
-            return new AuthenticationBuilder(services);
-        }
-
-        public static AuthenticationBuilder AddAuthentication(this IServiceCollection services, string defaultScheme)
-            => services.AddAuthentication(o => o.DefaultScheme = defaultScheme);
-
-        public static AuthenticationBuilder AddAuthentication(this IServiceCollection services, Action<AuthenticationOptions> configureOptions)
-        {
-            if (services == null)
-            {
-                throw new ArgumentNullException(nameof(services));
-            }
-
-            if (configureOptions == null)
-            {
-                throw new ArgumentNullException(nameof(configureOptions));
-            }
-
-            var builder = services.AddAuthentication();
-            services.Configure(configureOptions);
-            return builder;
-        }
-
-        // Used to ensure that there's always a sign in scheme
-        private class EnsureSignInScheme<TOptions> : IPostConfigureOptions<TOptions> where TOptions : RemoteAuthenticationOptions
-        {
-            private readonly AuthenticationOptions _authOptions;
-
-            public EnsureSignInScheme(IOptions<AuthenticationOptions> authOptions)
-            {
-                _authOptions = authOptions.Value;
-            }
-
-            public void PostConfigure(string name, TOptions options)
-            {
-                options.SignInScheme = options.SignInScheme ?? _authOptions.DefaultSignInScheme;
-            }
-        }
-
-        public static AuthenticationBuilder AddNullHandler(this AuthenticationBuilder builder, string authenticationScheme, string displayName, Action<NullAuthenticationOptions> configureOptions)
-        {
-            builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IPostConfigureOptions<NullAuthenticationOptions>, NullAuthenticationPostConfigureOptions>());
-            return builder.AddScheme<NullAuthenticationOptions, NullAuhtenticationHandler>(authenticationScheme, displayName, configureOptions);
-        }
-    }
-
-    public class NullAuthenticationPostConfigureOptions : IPostConfigureOptions<NullAuthenticationOptions>
-    {
-        public void PostConfigure(string name, NullAuthenticationOptions options)
-        {
-        }
-    }
-
-    public class NullAuthenticationOptions : AuthenticationSchemeOptions
-    {
-
-    }
-
-    public class NullAuhtenticationHandler : AuthenticationHandler<NullAuthenticationOptions>
-    {
-        public NullAuhtenticationHandler(IOptionsMonitor<NullAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
-        {
-        }
-
-        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
-        {
-            var principal = new ClaimsPrincipal();
-            var claims = new[]
-            {
-                new Claim("Granted", "yes"),
-                new Claim(ClaimTypes.Role, "Bar"),
-                new Claim(ClaimTypes.NameIdentifier, "Foo")
-            };
-
-            principal.AddIdentity(new ClaimsIdentity(claims, Scheme.Name));
-
-            var result = AuthenticateResult.Success(new AuthenticationTicket(principal, new AuthenticationProperties(), Scheme.Name));
-            return Task.FromResult(result);
-        }
-    }
-
     public class Startup : FunctionsStartup
     {
         public override void Configure(IFunctionsHostBuilder builder)
@@ -132,25 +39,12 @@ namespace ClientApiFunctionHost
                 .ReadFrom.Configuration(configuration)
                 .CreateLogger();
 
-            var services = builder.Services;
+            var services = new ServiceCollection();
 
             services.AddSingleton<DiagnosticSource>(new DiagnosticListener("Microsoft.AspNetCore"));
             services.AddSingleton<ObjectPoolProvider>(new DefaultObjectPoolProvider());
             services.AddSingleton<ILoggerFactory>(new SerilogLoggerFactory(Log.Logger, false));
             services.AddSingleton<IConfiguration>(configuration);
-
-            var issuer = configuration["TokenProviderOptions:Issuer"];
-            var audience = configuration["TokenProviderOptions:Audience"];
-
-            services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = "WebJobsAuthLevel";
-                    options.DefaultScheme = "WebJobsAuthLevel";
-                    options.DefaultChallengeScheme = "WebJobsAuthLevel";
-                })
-                .AddNullHandler("WebJobsAuthLevel", "WebJobsAuthLevel", options => { })
-                .AddNullHandler(JwtBearerDefaults.AuthenticationScheme, JwtBearerDefaults.AuthenticationScheme, options => { });
 
             var startup = new ClientApiStartup(configuration);
             startup.ConfigureServices(services);
